677a928b2ddd3f44f695d006b770ef4e5ae69cdb
Dike-Code <dikeclintonobinna58@gmail.com>
2023-04-21 19:45:55 -0700

    update

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,4 +1,9 @@
-# install flask from pip3 using puppe
+# install flask from pip3 using puppet
+# # install puppet-lint -v 2.1.0
+
+exec { 'puppet-lint':
+  command => '/usr/bin/apt-get -y install puppet-lint -v 2.1.0',
+}
 
 package  {'flask':
 ensure='2.1.0',
